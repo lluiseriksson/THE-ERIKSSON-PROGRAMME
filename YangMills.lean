5c4b616304aea08ac65ebd491df592352279f61a
lluiseriksson <lluiseriksson@gmail.com>
2026-04-24 11:42:06 +0200

    Expose ClusterRpowBridge in root module

--- YangMills.lean
+++ YangMills.lean
@@ -1,4 +1,4 @@
-﻿import YangMills.ClayCore.BalabanRG.P91CorrectedWindowPublicShim
+import YangMills.ClayCore.BalabanRG.P91CorrectedWindowPublicShim
 import YangMills.ClayCore.BalabanRG.P91CorrectedWindowConsumerPacket
 import YangMills.ClayCore.BalabanRG.P91LegacyRouteCounterexample
 import YangMills.ClayCore.BalabanRG.RGIncrementDecayP81ClusterExpansionWitness
@@ -188,3 +188,4 @@ import YangMills.L8_Terminal.ConnectedCorrDecayTheoremBundle
 import YangMills.ClayCore.SchurTwoSitePhase
 import YangMills.ClayCore.SchurOffDiagonal
 import YangMills.ClayCore.ConnectedCorrDecay
+import YangMills.ClayCore.ClusterRpowBridge
