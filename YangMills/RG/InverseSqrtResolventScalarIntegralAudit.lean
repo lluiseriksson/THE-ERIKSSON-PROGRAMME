2f1acdc12dd1cf91c49fda176ce893345da99df9
lluiseriksson <lluiseriksson@gmail.com>
2026-07-17 00:25:55 +0200

    Audit scalar Balakrishnan integrability

--- YangMills/RG/InverseSqrtResolventScalarIntegralAudit.lean
+++ YangMills/RG/InverseSqrtResolventScalarIntegralAudit.lean
@@ -6,3 +6,4 @@ import YangMills.RG.InverseSqrtResolventScalarIntegral
 
 #print axioms YangMills.RG.integral_inverseSqrtResolventScalarIntegrand_one
 #print axioms YangMills.RG.integral_inverseSqrtResolventScalarIntegrand
+#print axioms YangMills.RG.integrableOn_inverseSqrtResolventScalarIntegrand
