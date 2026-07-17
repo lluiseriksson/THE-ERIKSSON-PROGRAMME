7cd3d70da3402f95ccec709b90ebac4e95175e6f
lluiseriksson <lluiseriksson@gmail.com>
2026-07-17 20:47:02 +0200

    derive physical CMP99 sparse walk summability

---
+++ YangMills/RG/BalabanCMP99PatchedParametrixPhysicalSummabilityAudit.lean
@@ -0,0 +1,11 @@
+/- Copyright (c) 2026 Lluis Eriksson. All rights reserved.
+Released under the GNU Affero General Public License v3.0
+as described in the file LICENSE.
+Authors: Lluis Eriksson -/
+
+import YangMills.RG.BalabanCMP99PatchedParametrixPhysicalSummability
+
+/-! Axiom audit for physical sparse-walk summability. -/
+
+#print axioms YangMills.RG.norm_cmp99PhysicalAnchoredPatchWalk_term_le
+#print axioms YangMills.RG.summable_cmp116WeakenedPhysicalPatchWalkSeries_of_simpleDomainDictionary
