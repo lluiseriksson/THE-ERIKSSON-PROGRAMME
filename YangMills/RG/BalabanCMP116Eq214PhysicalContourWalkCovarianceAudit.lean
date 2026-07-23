222f29f72560401648f466966591cdb39b15b308
lluiseriksson <lluiseriksson@gmail.com>
2026-07-23 07:19:22 +0200

    install physical walk covariance in R1

---
+++ YangMills/RG/BalabanCMP116Eq214PhysicalContourWalkCovarianceAudit.lean
@@ -0,0 +1,13 @@
+/- Copyright (c) 2026 Lluis Eriksson. All rights reserved.
+Released under the GNU Affero General Public License v3.0
+as described in the file LICENSE.
+Authors: Lluis Eriksson -/
+
+import YangMills.RG.BalabanCMP116Eq214PhysicalContourWalkCovariance
+
+/-! # Axiom audit for the physical contour walk covariance -/
+
+#print axioms YangMills.RG.CMP116Eq214PhysicalContourDensity.withComplexPhysicalWalkCovariance_baseCovariance
+#print axioms YangMills.RG.CMP116Eq214PhysicalContourDensity.withComplexPhysicalWalkCovariance_contourCovariance
+#print axioms YangMills.RG.CMP116Eq214PhysicalContourDensity.withComplexPhysicalWalkCovariance_entry_cauchyBoundary
+#print axioms YangMills.RG.CMP116Eq214PhysicalContourDensity.withComplexPhysicalWalkGammaCovariance_r1Matrix
