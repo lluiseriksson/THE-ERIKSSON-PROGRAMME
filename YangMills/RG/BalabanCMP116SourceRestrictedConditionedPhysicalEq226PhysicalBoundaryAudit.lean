d987b7b9bb5215e469a9edc6b6184a8e91df5fae
lluiseriksson <lluiseriksson@gmail.com>
2026-07-25 18:22:59 +0200

    consume the physical interaction in equation 2.26

---
+++ YangMills/RG/BalabanCMP116SourceRestrictedConditionedPhysicalEq226PhysicalBoundaryAudit.lean
@@ -0,0 +1,3 @@
+import YangMills.RG.BalabanCMP116SourceRestrictedConditionedPhysicalEq226PhysicalBoundary
+
+#print axioms YangMills.RG.CMP116Eq214PhysicalContourDensity.norm_term_le_eq226SourceTermWeight_of_sourcePi4ConditionedPhysicalPotentialLedger
