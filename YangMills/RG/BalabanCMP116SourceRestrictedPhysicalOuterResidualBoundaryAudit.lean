6163e1f5c9c20a09bd3d91deb07d23d7561fd4a3
lluiseriksson <lluiseriksson@gmail.com>
2026-07-25 09:54:34 +0200

    close the physical residual outer boundary

---
+++ YangMills/RG/BalabanCMP116SourceRestrictedPhysicalOuterResidualBoundaryAudit.lean
@@ -0,0 +1,3 @@
+import YangMills.RG.BalabanCMP116SourceRestrictedPhysicalOuterResidualBoundary
+
+#print axioms YangMills.RG.CMP116Eq214PhysicalContourDensity.nestedCauchyBoundaryBound_of_sourcePi4PhysicalOuterResidual
