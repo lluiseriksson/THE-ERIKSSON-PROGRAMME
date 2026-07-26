38e091306301039bf41611f810d596079e0f9b9e
lluiseriksson <lluiseriksson@gmail.com>
2026-07-26 16:47:01 +0200

    construct continuous field derivatives of equation-80 domain jets

---
+++ YangMills/RG/BalabanCMP102Eq80SourcePi4ConnectedDomainFieldDerivativeAudit.lean
@@ -0,0 +1,31 @@
+/- Copyright (c) 2026 Lluis Eriksson. All rights reserved.
+Released under the GNU Affero General Public License v3.0
+as described in the file LICENSE.
+Authors: Lluis Eriksson -/
+
+import YangMills.RG.BalabanCMP102Eq80SourcePi4ConnectedDomainFieldDerivative
+
+#print axioms
+  YangMills.RG.fderiv_cmp102PartialPropagatorJet_at
+#print axioms
+  YangMills.RG.fderiv_cmp102PartialPropagatorJet_eq_fieldDerivative
+#print axioms
+  YangMills.RG.continuous_cmp102PartialPropagatorJetFieldDerivative_comp
+#print axioms
+  YangMills.RG.continuous_cmp102PartialPropagatorJet_comp
+#print axioms
+  YangMills.RG.cmp102Eq80SourcePi4FaaDiBrunoDomainChoiceTermAt_hasFDerivAt
+#print axioms
+  YangMills.RG.continuous_cmp102Eq80SourcePi4FaaDiBrunoDomainChoiceFieldDerivativeAt_comp
+#print axioms
+  YangMills.RG.continuous_cmp102Eq80SourcePi4FaaDiBrunoDomainChoiceTermAt_joint
+#print axioms
+  YangMills.RG.cmp102Eq80SourcePi4FaaDiBrunoDomainChoiceFieldDerivativeAt_zero_field
+#print axioms
+  YangMills.RG.cmp102Eq80SourcePi4FaaDiBrunoDomainCoefficientAt_hasFDerivAt_explicit
+#print axioms
+  YangMills.RG.continuous_cmp102Eq80SourcePi4FaaDiBrunoDomainCoefficientFieldDerivativeAt_comp
+#print axioms
+  YangMills.RG.continuous_cmp102Eq80SourcePi4FaaDiBrunoDomainCoefficientAt_joint
+#print axioms
+  YangMills.RG.cmp102Eq80SourcePi4FaaDiBrunoDomainCoefficientFieldDerivativeAt_zero_field
