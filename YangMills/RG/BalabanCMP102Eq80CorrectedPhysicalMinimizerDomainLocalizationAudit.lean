232344ff6a56f1c5c4899032450af4fb2c535d35
lluiseriksson <lluiseriksson@gmail.com>
2026-07-27 15:08:27 +0200

    specialize corrected minimizer to domain coefficients

---
+++ YangMills/RG/BalabanCMP102Eq80CorrectedPhysicalMinimizerDomainLocalizationAudit.lean
@@ -0,0 +1,4 @@
+import YangMills.RG.BalabanCMP102Eq80CorrectedPhysicalMinimizerDomainLocalization
+
+#print axioms YangMills.RG.cmp102Eq80CorrectedSourcePi4PhysicalDomainLocalizedMinimizerIncrement
+#print axioms YangMills.RG.tendsto_sum_cmp102Eq80CorrectedSourcePi4PhysicalDomainLocalizedMinimizerIncrements
