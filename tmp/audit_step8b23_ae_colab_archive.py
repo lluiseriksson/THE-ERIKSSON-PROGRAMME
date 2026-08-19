ae535072b34f60452396ec8a1cff1c8f6df3312b
lluiseriksson <lluiseriksson@gmail.com>
2026-08-19 17:24:11 +0200

    point A-E Colab notebook at v42

--- tmp/audit_step8b23_ae_colab_archive.py
+++ tmp/audit_step8b23_ae_colab_archive.py
@@ -18,8 +18,8 @@ import sys
 
 
 ROOT = Path(__file__).resolve().parents[1]
-SOURCE_SHA = "98fa7087e544b221f83236cacbeed3d53ada43cd"
-RUNNER_REV = "step8b23-ae-v41"
+SOURCE_SHA = "4616ce5474c0584f794a1d70da5a07564d4d480b"
+RUNNER_REV = "step8b23-ae-v42"
 MATHLIB_SHA = "07642720480157414db592fa85b626dafb71355b"
 EVIDENCE_ROOT = "hrpoly-step8b23-ae-evidence"
 
