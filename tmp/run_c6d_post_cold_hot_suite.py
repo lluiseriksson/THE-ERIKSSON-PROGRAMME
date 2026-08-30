7ff3c5e47e96adc2bfd6ac3ceff92efc0eb4beee
lluiseriksson <lluiseriksson@gmail.com>
2026-08-30 05:25:58 +0200

    Fix retained hot-suite child hash pin

--- tmp/run_c6d_post_cold_hot_suite.py
+++ tmp/run_c6d_post_cold_hot_suite.py
@@ -17,7 +17,7 @@ RUNNERS = [
         "uniform_certificate_from_value",
         "ba317e24fb2f7386bc3ab2262bc23322c041209f",
         "tmp/run_c6d_uniform_certificate_from_value_hot.py",
-        "697136d12204a3f0b7c00d8b210ebcd6e974acfc494d8e0087b622444851fb5e",
+        "697136d12204a3f0b7c00d8b210ebcd6e974accf494d8e0087b622444851fb5e",
     ),
     (
         "neumann_precision",
