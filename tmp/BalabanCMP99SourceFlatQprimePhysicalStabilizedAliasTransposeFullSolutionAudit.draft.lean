ae66aab36a4fdf4a4126ea593dcb9cde491d1968
lluiseriksson <lluiseriksson@gmail.com>
2026-09-01 10:52:05 +0200

    Draft the physical transposed Eq246 pullback

---
+++ tmp/BalabanCMP99SourceFlatQprimePhysicalStabilizedAliasTransposeFullSolutionAudit.draft.lean
@@ -0,0 +1,8 @@
+import YangMills.RG.BalabanCMP99SourceFlatQprimePhysicalStabilizedAliasTransposeFullSolution
+
+/-!
+# Axiom audit: physical arbitrary-source transposed Eq. (2.46) solution
+-/
+
+#print axioms YangMills.RG.cmp99SourceFlatQprimePhysicalStabilizedAliasTransposeFullSolution
+#print axioms YangMills.RG.cmp99SourceFlatQprimePhysicalAliasPrecisionMatrix_transpose_mulVec_fullSolution
